ae6553d4c14e8de8d3e71d756f0c5c16b52da476
young.chen <young.chen@nio.com>
2022-08-02 10:44:06 +0800

    add a line on dev1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("dev2 add");
+        System.out.println("dev1");
     }
 }
